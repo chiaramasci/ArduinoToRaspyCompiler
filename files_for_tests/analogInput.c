805c0ed0f54ab830218c068e2dc564aecf75d2e8
Chiara <chiaramasci98@gmail.com>
2019-06-03 11:10:32 +0200

    from printf to return in lex

--- files_for_tests/analogInput.c
+++ files_for_tests/analogInput.c
@@ -33,20 +33,20 @@ int sensorValue = 0; // variable to store the value coming from the sensor
 
 void setup()
 {
-    // declare the ledPin as an OUTPUT:
-    pinMode(ledPin, OUTPUT);
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop()
 {
-    // read the value from the sensor:
-    sensorValue = analogRead(sensorPin);
-    // turn the ledPin on
-    digitalWrite(ledPin, HIGH);
-    // stop the program for <sensorValue> milliseconds:
-    delay(sensorValue);
-    // turn the ledPin off:
-    digitalWrite(ledPin, LOW);
-    // stop the program for for <sensorValue> milliseconds:
-    delay(sensorValue);
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  // turn the ledPin on
+  digitalWrite(ledPin, HIGH);
+  // stop the program for <sensorValue> milliseconds:
+  delay(sensorValue);
+  // turn the ledPin off:
+  digitalWrite(ledPin, LOW);
+  // stop the program for for <sensorValue> milliseconds:
+  delay(sensorValue);
 }
