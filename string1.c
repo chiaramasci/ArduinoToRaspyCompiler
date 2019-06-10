0e912ff7b72a6692a947f87ff806cbe1eaa8e5d0
Chiara <chiaramasci98@gmail.com>
2019-06-10 09:41:30 +0200

    Non so cosa ho fatto ma funziona

--- string1.c
+++ string1.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
 #include <string.h>
-void func(char str[])
+char *func(char *str)
 {
+    char result[100];
 
     /* To print string in upperCase*/
     for (int i = 0; i <= strlen(str); i++)
     {
         if (str[i] >= 97 && str[i] <= 122)
-            str[i] = str[i] - 32;
+            result[i] = str[i] - 32;
     }
-    printf("\n\nString in Uppercase: %s", str);
+
+    return result;
 }
 
-void concatenate(char first[], char second[], char third[])
+char *concatenate(char first[], char second[], char third[])
 {
     char result[100];
 
     strcpy(result, first);
     strcat(result, second);
     strcat(result, third);
-
-    printf("%s", result);
+    return result;
 }
 
 void main()
@@ -33,7 +34,8 @@ void main()
     char a[] = "ciao";
     char b[] = "so";
     char c[] = "parlare";
-    concatenate(a, b, c);
+    char *concatenated = concatenate(a, b, c);
 
-    func(result);
+    char *uppercase = func(concatenated);
+    printf("%s", uppercase);
 }
